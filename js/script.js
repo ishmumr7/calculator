16c1f2fb8314baa68a58a73b0d0f1c6e00eddd5d
Tahzeeb <ishmum20000@gmail.com>
2022-09-28 18:46:18 +0600

    Add operate function

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+const operate = (operator, num1, num2) => {
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        
+        case '-':
+            return subtract(num1, num2);
+
+        case '*':
+            return multiply(num1, num2);
+
+        case '/':
+            return divide(num1, num2);
+    }
+}
